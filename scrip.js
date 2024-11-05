b3aaa1983516a9f335a9f5fcee0268398da09f03
jamshidbek <oktamovjamshid20@gmail.com>
2024-11-04 23:33:56 -0800

    salom yangi malumot

---
+++ scrip.js
@@ -0,0 +1,27 @@
+// const theif = {
+//     jeced: "black",
+//     height: 1.2,
+//     color:{
+//         hair: "gray",
+//         style: "curley"
+//     }
+// }
+
+// for (let key in theif){
+//     if(typeof theif[key] === "object"){
+//         for (let i in theif[key])
+//         console.log(`property ${i} value ${theif[key][i]}`)
+//     }
+//     else{
+//         console.log(`property ${key} value ${theif[key]}`)
+//     }
+// }
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// arr.pop(); // bu methot oxiridan ochiradi
+// arr.push(7); // bu methot oxirdan qoshib beradi
+
+// arr.length; // bu method massiv larni ichidagi uzunligini qaytaradi lekin bu ichiga kirib sanamaydi faqatgina oxirgi elementni indexga 1 ni qoshib chiqaradi
+// console.log(arr.pop());
+// console.log(arr);
